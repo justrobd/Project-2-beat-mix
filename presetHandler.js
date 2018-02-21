5d6f49f98fcc4780f5b00870c5be6926f74eb62c
Robert DeBartolo <robertdebartolo@gmail.com>
2018-02-21 10:31:40 -0600

    Add functionality I looked at the solution to solve

--- presetHandler.js
+++ presetHandler.js
@@ -1,8 +1,39 @@
 // Use this presets array inside your presetHandler
 const presets = require('./presets');
 
+const getPreset = (index) => {
+	return presets[index] || null;
+};
+
+const createOrUpdatePreset = (index, array) => {
+	if(!presets[index]) return;
+	presets[index] = array;
+	return presets[index];
+};
+
 // Complete this function:
-const presetHandler = () => {};
+const presetHandler = (method, index, newPresetsArray) => {
+             
+	if(method === 'GET' ) {
+          let preset = getPreset(index);
+	  if(preset) {
+		  return [200, preset];
+	  } else {
+		  return [404];
+	  }
+	} else if(method === 'PUT') {
+          const newPreset = createOrUpdatePreset(index, newPresetsArray);
+	  if(newPreset) {
+		  return [200, newPreset]; 
+	  } else {
+		  return [404];
+	  }
+	} else {
+		return [400];
+	}
+
+
+};
 
 // Leave this line so that your presetHandler function can be used elsewhere:
 module.exports = presetHandler;
